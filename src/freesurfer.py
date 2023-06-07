d1fc3a6e37660b6507c02bdd331746e6b2cc0e5c
Derek Berger <dmberger.dev@gmail.com>
2023-06-07 07:59:20 -0400

    implement parsing of .stats files

---
+++ src/freesurfer.py
@@ -0,0 +1,163 @@
+from __future__ import annotations
+
+# fmt: off
+import sys  # isort: skip
+from pathlib import Path  # isort: skip
+ROOT = Path(__file__).resolve().parent.parent  # isort: skip
+sys.path.append(str(ROOT))  # isort: skip
+# fmt: on
+
+from io import StringIO
+import os
+import sys
+from argparse import ArgumentParser, Namespace
+from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    List,
+    Optional,
+    Sequence,
+    Tuple,
+    Union,
+    cast,
+    no_type_check,
+)
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from dataclasses import dataclass
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+from numpy import ndarray
+from pandas import DataFrame, Series
+
+
+from typing import TextIO
+
+"""
+See https://github.com/fphammerle/freesurfer-stats/blob/master/freesurfer_stats/__init__.py
+for motivation and reasoning behind code below.
+"""
+
+
+@dataclass
+class ColumnInfo:
+    shortname: str  # ColHeader
+    name: str  # FileName
+    unit: str  # Units
+    index: int
+
+
+@dataclass
+class MetaData:
+    subjectname: str
+    filename: Path
+    annot: Path
+
+
+def parse_comment(line: str) -> str:
+    if not line.startswith("# "):
+        raise ValueError("Not a comment line")
+    return line[2:]
+
+
+def parse_colnames(line: str) -> list[str]:
+    if not line.startswith("# "):
+        raise ValueError("Not a table header line")
+    line = line[2:].rstrip()
+    return line.split(" ")
+
+
+def parse_metadata(lines: list[str], fname: Path) -> MetaData:
+    end = -1
+    for i, line in enumerate(lines):
+        if line.startswith("# NTableCols"):
+            end = i
+            break
+    lines = lines[:end]
+
+    subject, annot = "", ""
+    for line in lines:
+        if "subjectname" in line:
+            subject = line.replace("# subjectname ", "").strip()
+        elif "# AnnotationFile " in line:
+            annot = line.replace("# AnnotationFile ", "").strip()
+        else:
+            continue
+    return MetaData(
+        subjectname=subject,
+        filename=Path(fname),
+        annot=Path(annot),
+    )
+
+
+def parse_table(lines: list[str]) -> DataFrame:
+    start = 0
+    for i, line in enumerate(lines):
+        if line.startswith("# ColHeaders"):
+            start = i
+    lines = lines[start:]
+    lines[0] = lines[0].replace("# ColHeaders ", "")
+    text = "\n".join(lines)
+
+    return pd.read_table(StringIO(text), sep="\s+")
+
+
+def parse_table_metadata(lines: list[str]) -> list[ColumnInfo]:
+    lines = list(filter(lambda line: "# TableCol" in line, lines))
+    indices = list(range(0, len(lines), 3))
+    triples = []
+    for i in range(len(indices) - 1):
+        start = indices[i]
+        stop = indices[i + 1]
+        triples.append(tuple(lines[start:stop]))
+    infos = []
+    for triple in triples:
+        infos.append(parse_table_metadata_lines(triple))
+    return infos
+
+
+def parse_table_metadata_lines(triple: tuple[str, str, str]) -> ColumnInfo:
+    shortname, name, unit, index = "", "", "", ""
+    for line in triple:
+        if not line.startswith("# TableCol"):
+            raise ValueError("Not a table column header line")
+        line = line.replace("# TableCol  ", "")
+        try:
+            index, fieldname, *values = line.split(" ")
+            value = " ".join(values).strip()
+        except ValueError as e:
+            raise RuntimeError(f"Could not parse line: {line}") from e
+
+        if fieldname == "ColHeader":
+            shortname = value
+        elif fieldname == "FieldName":
+            name = value
+        elif fieldname == "Units":
+            unit = value
+        else:
+            raise ValueError(f"Unrecognized table header line: `{line}`")
+    return ColumnInfo(shortname=shortname, name=name, unit=unit, index=int(index))
+
+
+class FreesurferStats:
+    def __init__(self) -> None:
+        self.header: str
+
+
+TEST = ROOT / "data/ABIDE-I/Caltech_0051457/stats/lh.aparc.stats"
+
+if __name__ == "__main__":
+    with open(TEST, "r") as handle:
+        all_lines = handle.readlines()
+    infos = parse_table_metadata(all_lines)
+    for info in infos:
+        print(info)
+    print(parse_table(all_lines))
+    print(parse_metadata(all_lines, Path(TEST)))
