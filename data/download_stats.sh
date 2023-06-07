a96b8581f59cf9f51148223c8e92ccc007ae53bc
Derek Berger <dmberger.dev@gmail.com>
2023-06-06 23:53:13 -0400

    include more downloads

--- data/download_stats.sh
+++ data/download_stats.sh
@@ -24,6 +24,14 @@ awscli="$SCRATCH/aws-cli/v2/2.11.25/dist/aws"
     s3://fcp-indi/data/Projects/ABIDE_Initiative/ \
     ABIDE-II
 
+"$awscli" s3 cp --no-sign-request \
+    s3://fcp-indi/data/Projects/ABIDE_Initiative/Phenotypic_V1_0b.csv \
+    ABIDE-II
+
+"$awscli" s3 cp --no-sign-request \
+    s3://fcp-indi/data/Projects/ABIDE_Initiative/Phenotypic_V1_0b_preprocessed.csv \
+    ABIDE-II
+
 
 "$awscli" s3 sync --no-sign-request \
     s3://fcp-indi/data/Projects/ABIDE_Initiative/Resources/ \
@@ -32,3 +40,10 @@ awscli="$SCRATCH/aws-cli/v2/2.11.25/dist/aws"
 "$awscli" s3 sync --no-sign-request \
     s3://fcp-indi/data/Projects/ABIDE_Initiative/PhenotypicData/ \
     ABIDE-II/phenotypic_data
+
+wget http://fcon_1000.projects.nitrc.org/indi/abide/ABIDEII_Data_Legend.pdf
+
+# see http://fcon_1000.projects.nitrc.org/indi/abide/abide_II.html
+# for links to download ABIDE-II phenotypic data:
+# https://www.nitrc.org/frs/downloadlink.php/9108
+# https://www.nitrc.org/frs/downloadlink.php/9109
