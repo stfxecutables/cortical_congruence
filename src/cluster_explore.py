ac39b87d7086a07b8c5d3d991393dabc57eff640
Derek Berger <dmberger.dev@gmail.com>
2023-07-02 21:31:40 -0400

    implement random feature evaluation

--- src/cluster_explore.py
+++ src/cluster_explore.py
@@ -13,7 +13,9 @@ import sys
 from argparse import Namespace
 from dataclasses import dataclass
 from io import StringIO
+from math import ceil
 from pathlib import Path
+from random import shuffle
 from typing import Callable, cast
 
 import numpy as np
@@ -72,7 +74,17 @@ def compute_regression_metrics(args: Namespace) -> DataFrame:
     model = args.model
     modelname = args.modelname
     target = args.target
-    x = dfr.filter(regex="CMC").to_numpy()
+    rand = args.random_feats
+    perc_max = args.perc_max
+    x = dfr.filter(regex=args.feature_regex)
+    if rand:
+        idx = np.random.permutation(x.shape[1])
+        n = np.random.randint(5, max(6, ceil(perc_max * x.shape[1])))
+        x = x.iloc[:, idx].iloc[:, :n]
+    else:
+        n = x.shape[1]
+
+    x = x.to_numpy()
     y = dfr[target]
     results = cross_validate(
         model,
@@ -91,47 +103,80 @@ def compute_regression_metrics(args: Namespace) -> DataFrame:
         },
         inplace=True,
     )
+    df.drop(columns=["fit_time", "score_time"], inplace=True)
     df.loc[:, "MAE"] = df["MAE"].abs()
     df.loc[:, "R2"] = df["R2"].abs()
     df.loc[:, "Exp.Var"] = df["Exp.Var"].abs()
     df["target"] = str(target).replace("TARGET__", "")
     df["model"] = modelname
+    df["n_feats"] = n
     return df
 
 
-def get_reg_args(reduce_cmc: bool, reduce_targets: bool = True) -> list[Namespace]:
+def get_reg_args(
+    feature_regex: str = "CMC",
+    reduce_cmc: bool = False,
+    reduce_targets: bool = True,
+    random_feats: bool = False,
+    n_random: int = 250,
+    perc_max: float = 0.25,
+) -> list[Namespace]:
     n_jobs = 1
     models = {
         "LR": lambda: LR(n_jobs=n_jobs),
-        "MLP": lambda: MLP(
-            hidden_layer_sizes=[32, 64, 128, 256],
-            activation="relu",
-            solver="adam",
-            alpha=1e-4,
-            shuffle=True,
-            learning_rate_init=3e-4,
-            max_iter=500,
-        ),
-        "LGB": lambda: LGB(n_jobs=n_jobs),
-        "KNN-1": lambda: KNN(n_neighbors=1, n_jobs=n_jobs),
-        "KNN-3": lambda: KNN(n_neighbors=3, n_jobs=n_jobs),
-        "KNN-9": lambda: KNN(n_neighbors=9, n_jobs=n_jobs),
+        # "MLP": lambda: MLP(
+        #     hidden_layer_sizes=[32, 64, 128, 256],
+        #     activation="relu",
+        #     solver="adam",
+        #     alpha=1e-4,
+        #     shuffle=True,
+        #     learning_rate_init=3e-4,
+        #     max_iter=500,
+        # ),
+        # "LGB": lambda: LGB(n_jobs=n_jobs),
+        # "KNN-1": lambda: KNN(n_neighbors=1, n_jobs=n_jobs),
+        # "KNN-3": lambda: KNN(n_neighbors=3, n_jobs=n_jobs),
+        # "KNN-9": lambda: KNN(n_neighbors=9, n_jobs=n_jobs),
         "Dummy": lambda: Dummy(strategy="mean"),
     }
     df = load_HCP_complete(
         focus=PhenotypicFocus.All, reduce_targets=reduce_targets, reduce_cmc=reduce_cmc
     )
-    dfr = df.filter(regex="CMC|TARGET|DEMO")
+    dfr = df.copy()
     args = []
     for target in dfr.filter(regex="TARGET").columns:
         for model in models:
-            args.append(
-                Namespace(
-                    **dict(
-                        data=dfr, model=models[model](), modelname=model, target=target
+            if not random_feats:
+                args.append(
+                    Namespace(
+                        **dict(
+                            data=dfr,
+                            model=models[model](),
+                            modelname=model,
+                            target=target,
+                            feature_regex=feature_regex,
+                            random_feats=False,
+                            perc_max=perc_max,
+                        )
                     )
                 )
-            )
+            else:
+                for _ in range(n_random):
+                    args.append(
+                        Namespace(
+                            **dict(
+                                data=dfr,
+                                model=models[model](),
+                                modelname=model,
+                                target=target,
+                                feature_regex=feature_regex,
+                                random_feats=True,
+                                perc_max=perc_max,
+                            )
+                        )
+                    )
+
+    shuffle(args)
     return args
 
 
@@ -139,12 +184,26 @@ if __name__ == "__main__":
     # pd.options.display.max_rows = 500
     # load_HCP_complete.clear()
     reduce_cmc = False
-    args = get_reg_args(reduce_cmc=reduce_cmc)
+    feature_regex = "FS"
+    random_feats = True
+    n_random = 500
+    perc_max = 0.1
+
+    args = get_reg_args(
+        feature_regex=feature_regex,
+        reduce_cmc=reduce_cmc,
+        random_feats=random_feats,
+        n_random=n_random,
+        perc_max=perc_max,
+    )
     r = "reduced" if reduce_cmc else "raw"
-    out = TABLES / f"quick_regression_cluster_targets_cmc_{r}.parquet"
+    f = feature_regex.lower()
+    rnd = f"_rand_{n_random}_of{perc_max:0.2f}p" if random_feats else ""
+    out = TABLES / f"quick_regression_cluster_targets_{f}_{r}{rnd}.parquet"
     if out.exists():
         df = pd.read_parquet(out)
     else:
+        print(f"Computing {len(args)} iterations:")
         all_results = Parallel(n_jobs=-1, verbose=10)(
             delayed(compute_regression_metrics)(arg) for arg in args
         )
@@ -157,10 +216,15 @@ if __name__ == "__main__":
         df.groupby("target")
         .apply(lambda grp: grp[grp.model == "Dummy"]["MAE"])
         .droplevel(1)
+        .drop_duplicates()
     )
     base_mae = df.target.apply(lambda target: dummy_maes[target])
-    df.insert(2, "sMAE", df["MAE"] / base_mae)
-    df = df.applymap(lambda x: np.nan if (isinstance(x, float) and x > 10) else x)
-    print(df.to_markdown(tablefmt="simple", index=False, floatfmt="0.4f"))
+    df.insert(3, "sMAE", df["MAE"] / base_mae)
+    best = df[df["sMAE"] < 1.0]
+    print(
+        best.sort_values(by="sMAE", ascending=False).to_markdown(
+            tablefmt="simple", index=False, floatfmt="0.4f"
+        )
+    )
 
     # print_correlations()
