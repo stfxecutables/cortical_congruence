a7e4c728f502f99fdb2505c1f9837db5e152232b
Derek Berger <dmberger.dev@gmail.com>
2023-07-22 10:34:03 -0400

    implement binned stratified kfold

--- src/feature_selection/kfold.py
+++ src/feature_selection/kfold.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 
+from pathlib import Path
+
+from numpy.random import RandomState
+
 # fmt: off
 import sys  # isort: skip
-from pathlib import Path  # isort: skip
 ROOT = Path(__file__).resolve().parent.parent.parent  # isort: skip
 sys.path.append(str(ROOT))  # isort: skip
 # fmt: on
@@ -17,6 +20,7 @@ from typing import (
     Any,
     Callable,
     Dict,
+    Iterator,
     List,
     Optional,
     Sequence,
@@ -33,6 +37,10 @@ from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 from numpy import ndarray
 from pandas import DataFrame, Series
+from sklearn._typing import ArrayLike, Int, MatrixLike
+from sklearn.model_selection import StratifiedKFold
+from sklearn.utils.multiclass import type_of_target
+from sklearn.utils.validation import column_or_1d
 from typing_extensions import Literal
 
 """key is metric name, tuple[float, ...] is fold values"""
@@ -50,3 +58,40 @@ class NestedKfold:
     """
 
     pass
+
+
+class BinStratifiedKFold(StratifiedKFold):
+    def __init__(
+        self,
+        n_splits: Int = 5,
+        *,
+        n_bins: int = 10,
+        shuffle: bool = False,
+        random_state: Int | RandomState | None = None,
+    ) -> None:
+        super().__init__(n_splits, shuffle=shuffle, random_state=random_state)
+        self.n_bins = n_bins
+
+    def split(
+        self, X: MatrixLike | list[str], y: ArrayLike, groups: Any = None
+    ) -> Iterator[Any]:
+        y = np.asarray(y)
+        y_type = type_of_target(y)
+        allowed = ("binary", "multiclass", "continuous")
+        if y_type not in allowed:
+            raise ValueError(
+                f"Supported target types are: {allowed}. Got {y_type!r} instead."
+            )
+
+        if y_type in ("binary", "multiclass"):
+            return super().split(X, y, groups)
+
+        # continuous case
+        y = column_or_1d(y)
+        y_cls = np.copy(y)
+        bin_edges = np.percentile(y, np.linspace(0, 100, self.n_bins + 1, endpoint=True))
+        for i in range(self.n_bins):
+            lo, hi = bin_edges[i], bin_edges[i + 1]
+            idx = (y >= lo) & (y < hi)
+            y_cls[idx] = i
+        return super().split(X, y_cls, groups)
