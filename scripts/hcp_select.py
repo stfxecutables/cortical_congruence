0305b7264429dd4dee42c5d70dbc594a0964b966
Derek Berger <dmberger.dev@gmail.com>
2023-07-22 16:00:25 -0400

    WIP

--- scripts/hcp_select.py
+++ scripts/hcp_select.py
@@ -1,5 +1,13 @@
 from __future__ import annotations
 
+# fmt: off
+import sys  # isort: skip
+from pathlib import Path  # isort: skip
+ROOT = Path(__file__).resolve().parent.parent  # isort: skip
+sys.path.append(str(ROOT))  # isort: skip
+# fmt: on
+
+
 import os
 
 import pandas as pd
