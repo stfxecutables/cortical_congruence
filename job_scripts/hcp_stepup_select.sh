642cfaa1da4794fb2155e38701f306cffbf6f3b2
Derek Berger <dmberger.dev@gmail.com>
2023-07-22 15:56:59 -0400

    write job scripts

---
+++ job_scripts/hcp_stepup_select.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --account=def-jlevman
+#SBATCH --mail-user=dberger@stfx.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=TIME_LIMIT_90
+#SBATCH --profile=all
+#SBATCH --job-name=hcp_select
+#SBATCH --array=0-2
+#SBATCH --output=hcp_select_%A_%a_%j.out
+#SBATCH --time=00-08:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=80
+
+SCRATCH="$(readlink -f "$SCRATCH")"
+PROJECT="$SCRATCH/cortical_congruence"
+PYTHON="$PROJECT/.venv/bin/python"
+
+PY_SCRIPTS="$PROJECT/scripts"
+PY_SCRIPT="$(readlink -f "$PY_SCRIPTS/hcp_select.py")"
+
+"$PYTHON" "$PY_SCRIPT"
---
+++ scripts/hcp_select.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+import os
+
+import pandas as pd
+
+from src.analysis.feature_select.stepwise import (
+    means_to_best_n_feats,
+    nested_results_to_means,
+    nested_stepup_feature_select,
+)
+from src.enumerables import FeatureRegex, FreesurferStatsDataset, RegressionMetric
+
+if __name__ == "__main__":
+    task = os.environ.get("SLURM_ARRAY_TASK_ID", None)
+    if task is None:
+        raise EnvironmentError("SLURM_ARRAY_TASK_ID is not defined.")
+    idx = int(task)
+    feature_regex = [*FeatureRegex][idx]
+
+    info = nested_stepup_feature_select(
+        dataset=FreesurferStatsDataset.HCP,
+        feature_regex=feature_regex,
+        max_n_features=50,
+        bin_stratify=True,
+        inner_progress=True,
+        use_outer_cached=True,
+        use_inner_cached=True,
+    )
+    pd.options.display.max_rows = 500
+    means = nested_results_to_means(info)
+    bests = means_to_best_n_feats(means, metric=RegressionMetric.ExplainedVariance)
+    print(bests.round(3))
+    final = (
+        bests.groupby(bests.columns.to_list()[:3])
+        .mean(numeric_only=True)
+        .drop(columns="outer_fold")
+        .rename(columns={"selection_iter": "mean_n_iter"})
+        .sort_values(by=RegressionMetric.ExplainedVariance.value)
+    )
+    print(final.round(3))
