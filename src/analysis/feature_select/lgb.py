89c11d3cba228685bc34a85268dad3274ddbb6fb
Derek Berger <dmberger.dev@gmail.com>
2023-07-25 17:48:37 -0400

    wip - implement LGBM selection

--- src/analysis/feature_select/lgb.py
+++ src/analysis/feature_select/lgb.py
@@ -8,38 +8,155 @@ sys.path.append(str(ROOT))  # isort: skip
 # fmt: on
 
 
+import json
 import sys
+from dataclasses import dataclass
 from pathlib import Path
-from typing import cast
+from typing import Mapping
 
 import numpy as np
 import pandas as pd
+from joblib import Parallel, delayed
+from lightgbm import LGBMClassifier, LGBMRegressor
+from lightgbm.sklearn import LGBMClassifier, LGBMRegressor
 from numpy import ndarray
 from pandas import DataFrame
-from sklearn.linear_model import LassoCV
-from sklearn.model_selection import cross_validate
+from sklearn.model_selection import ParameterGrid, cross_validate
 from tqdm import tqdm
 
 from src.analysis.feature_select.preprocessing import drop_target_nans, load_preprocessed
 from src.analysis.utils import get_kfold
 from src.constants import CACHED_RESULTS, PBAR_COLS, PBAR_PAD, TABLES, ensure_dir
 from src.enumerables import (
+    ClassificationMetric,
     FeatureRegex,
     FreesurferStatsDataset,
     RegressionMetric,
     RegressionModel,
 )
 
-LASSO_RESULTS = ensure_dir(TABLES / "lasso_results")
-LASSO_CACHE = ensure_dir(CACHED_RESULTS / "lasso_selection")
+LGBM_RESULTS = ensure_dir(TABLES / "lgbm_results")
+LGBM_CACHE = ensure_dir(CACHED_RESULTS / "lgbm_selection")
+
+DEFAULTS = dict(
+    # boosting_type="rf",
+    num_leaves=31,
+    max_depth=-1,
+    min_child_samples=20,
+    # subsample=1.0,
+    n_estimators=100,
+    reg_alpha=0.0,
+    n_jobs=-1,
+    verbose=-1,
+)
 
 
-def lasso_select_target(
+@dataclass
+class FitArgs:
+    lgb_args: Mapping
+    X_train: ndarray
+    X_test: ndarray
+    y_train: ndarray
+    y_test: ndarray
+    is_reg: bool
+    bin_stratify: bool
+    iter: int
+
+
+def fit_lgb(fit_args: FitArgs) -> DataFrame:
+    defaults = DEFAULTS
+
+    param = fit_args.lgb_args
+    is_reg = fit_args.is_reg
+    X_train = fit_args.X_train
+    X_test = fit_args.X_test
+    y_train = fit_args.y_train
+    y_test = fit_args.y_test
+    bin_stratify = fit_args.bin_stratify
+    tune_iter = fit_args.iter
+
+    args = {**defaults, **param}
+
+    estimator = LGBMRegressor(**args) if is_reg else LGBMClassifier(**args)
+    metric_cls = RegressionMetric if is_reg else ClassificationMetric
+    scorers = metric_cls.scorers()
+    cv = get_kfold(bin_stratify=bin_stratify, is_reg=is_reg)
+    results = DataFrame(
+        cross_validate(
+            estimator=estimator,  # type: ignore
+            X=X_train,
+            y=y_train,
+            scoring=scorers,
+            cv=cv,
+            n_jobs=1,
+            return_train_score=True,
+            return_estimator=True,
+        )
+    )
+    estimators = results["estimator"].to_list()
+    imps = [json.dumps(est.feature_importances_.tolist()) for est in estimators]
+    results.drop(columns=["estimator", "fit_time", "score_time"], inplace=True)
+    if is_reg:
+        for metric in RegressionMetric.inverted():
+            train = f"train_{metric.value}"
+            test = f"test_{metric.value}"
+            results[train] *= -1
+            results[test] *= -1
+    results.rename(columns=lambda s: f"inner_{s}", inplace=True)
+
+    estimator = LGBMRegressor(**args) if is_reg else LGBMClassifier(**args)
+    estimator.fit(X_train, y_train)
+    y_pred_train = estimator.predict(X_train)
+    y_pred_test = estimator.predict(X_test)
+
+    for metric in metric_cls:
+        results[f"outer_train_{metric.value}"] = metric(y_train, y_pred_train)
+    for metric in metric_cls:
+        results[f"outer_test_{metric.value}"] = metric(y_test, y_pred_test)
+
+    results["params"] = json.dumps(param)
+    results["inner_importances"] = imps
+    results["outer_importances"] = json.dumps(estimator.feature_importances_.tolist())
+    results.insert(0, "inner_fold", range(1, 6))
+    results.insert(1, "tune_iter", tune_iter)
+
+    return results
+
+
+def get_params(log: bool = False) -> list[dict]:
+    max_depth = 4
+    depths = list(range(1, max_depth + 1)) if max_depth > -1 else [-1]
+    num_leaves = list(range(2, min(64, int(2**max_depth)))) if max_depth > -1 else [64]
+    params = np.array(
+        ParameterGrid(
+            dict(
+                num_leaves=num_leaves,  # less than 2**max_depth
+                # max_depth=[-1, 1, 2, 3, 4, 5, 6],
+                max_depth=depths,
+                min_child_samples=[5, 25, 100],
+                # subsample=[0.05, 0.2, 0.4, 0.6, 0.8, 1.0],
+                subsample=[0.05, 0.2, 0.4, 0.6, 0.8, 0.9],  # for rf
+                subsample_freq=[1],  # for rf
+                n_estimators=[100],
+                reg_alpha=np.logspace(-3, 2, num=10, base=10).tolist(),
+                n_jobs=[1],
+            )
+        )
+    )
+    params = params[np.random.permutation(len(params))]
+    if log:
+        print(f"n_params: {len(params)}")
+    return params.tolist()
+
+
+def lgbm_select_target(
     df_train: DataFrame,
     df_test: DataFrame,
     target: str,
     regex: FeatureRegex,
+    scoring: tuple[RegressionMetric, ClassificationMetric],
     bin_stratify: bool,
+    n_tune: int = 8,
 ) -> DataFrame:
     feats = np.array(df_train.filter(regex=regex.value).columns.to_list())
     X_train = df_train.filter(regex=regex.value).to_numpy()
@@ -50,57 +167,40 @@ def lasso_select_target(
     X_train = np.asfortranarray(X_train)
     X_test = np.asfortranarray(X_test)
 
-    cv = get_kfold(bin_stratify=bin_stratify, is_reg=True)
-    estimator: LassoCV = cast(LassoCV, RegressionModel.Lasso.get())
-    results = DataFrame(
-        cross_validate(
-            estimator,
-            X_train,
-            y_train,
-            scoring=RegressionMetric.scorers(),
-            cv=cv,
-            n_jobs=-1,
-            return_train_score=True,
-            return_estimator=True,
+    is_reg = "REG" in target
+
+    params = get_params()
+    params = params[:n_tune]
+    args = [
+        FitArgs(
+            lgb_args=param,
+            X_train=X_train,
+            X_test=X_test,
+            y_train=y_train,
+            y_test=y_test,
+            is_reg=is_reg,
+            bin_stratify=bin_stratify,
+            iter=tune_iter,
         )
+        for tune_iter, param in enumerate(params)
+    ]
+
+    fits: list[DataFrame]
+    fits = Parallel(n_jobs=-1, verbose=0)(delayed(fit_lgb)(arg) for arg in args)  # type: ignore  # noqa
+    # results = [fit_lgb(arg) for arg in args]  # debug
+    tune_results = pd.concat(fits, axis=0, ignore_index=True)
+    means = (
+        tune_results.groupby("tune_iter")
+        .mean(numeric_only=True)
+        .drop(columns="inner_fold")
     )
-    estimators = results["estimator"].to_list()
-    results.drop(columns=["estimator", "fit_time", "score_time"], inplace=True)
-    for metric in RegressionMetric.inverted():
-        train = f"train_{metric.value}"
-        test = f"test_{metric.value}"
-        results[train] *= -1
-        results[test] *= -1
-    results.rename(columns=lambda s: f"inner_{s}", inplace=True)
+    reg_sort = f"inner_test_{scoring[0].value}"
+    cls_sort = f"inner_test_{scoring[1].value}"
+    sorter = reg_sort if is_reg else cls_sort
+    ascending = is_reg
 
-    tuned_alphas = [est.alpha_ for est in estimators]
-    # each of w has shape (X_train.shape[1],)
-    ws = [est.coef_ for est in estimators]
-    eps = 1e-15  # we should probably use np.spacing, or sthg...
-    selected_feats = []
-    for w in ws:
-        if np.sum(w) < eps:
-            selected_feats.append("none")
-        else:
-            idx = np.abs(w) > eps
-            selected_feats.append(str(feats[idx]))
-
-    estimator: LassoCV = cast(LassoCV, RegressionModel.Lasso.get())
-    estimator.fit(X_train, y_train)
-    y_pred_test: ndarray = estimator.predict(X_test)  # type: ignore
-    y_pred_train: ndarray = estimator.predict(X_train)  # type: ignore
-    for metric in RegressionMetric:
-        results[f"outer_train_{metric.value}"] = metric(y_train, y_pred_train)
-    for metric in RegressionMetric:
-        results[f"outer_test_{metric.value}"] = metric(y_test, y_pred_test)
-
-    outer_alpha = estimator.alpha_
-    outer_w = estimator.coef_
-    if np.sum(outer_w) < eps:
-        outer_selected_feats = "none"
-    else:
-        outer_idx = np.abs(outer_w) > eps
-        outer_selected_feats = str(feats[outer_idx])
+    best_idx = means.sort_values(by=sorter, ascending=ascending).head(10).index
+    results = tune_results.loc[best_idx]
 
     addons = {
         0: ("source", regex.value),
@@ -110,18 +210,17 @@ def lasso_select_target(
     for position, (colname, value) in addons.items():
         results.insert(position, colname, value)
 
-    results["inner_alpha"] = tuned_alphas
-    # results["inner_w"] = ws
-    results["inner_selected"] = selected_feats
-    results["outer_alpha"] = outer_alpha
-    # results["outer_w"] = Series([outer_w])
-    results["outer_selected"] = outer_selected_feats
     return results
 
 
-def nested_lasso_feature_select(
+def nested_lgbm_feature_select(
     dataset: FreesurferStatsDataset,
     feature_regex: FeatureRegex,
+    scoring: tuple[RegressionMetric, ClassificationMetric] = (
+        RegressionMetric.ExplainedVariance,
+        ClassificationMetric.BalancedAccuracy,
+    ),
+    n_tune: int = 128,
     bin_stratify: bool = True,
     use_cached: bool = True,
     load_complete: bool = False,
@@ -162,13 +261,13 @@ def nested_lasso_feature_select(
     regex = feature_regex.value
 
     r = regex.replace("|", "+")
-    fbase = f"{dataset.value}_{r}_lasso_selected"
-    all_fold_out = LASSO_RESULTS / f"{fbase}_nested.parquet"
+    fbase = f"{dataset.value}_{r}_lgbm_selected_htune={n_tune}"
+    all_fold_out = LGBM_RESULTS / f"{fbase}_nested.parquet"
     if use_cached and load_complete and all_fold_out.exists():
         return pd.read_parquet(all_fold_out)
 
     df = load_preprocessed(
-        dataset=dataset, regex=feature_regex, models=RegressionModel.Lasso
+        dataset=dataset, regex=feature_regex, models=RegressionModel.LightGBM
     )
 
     all_fold_infos = []
@@ -190,7 +289,7 @@ def nested_lasso_feature_select(
         y = df_select[target]
         all_fold_results: list[DataFrame] = []
         tname = str(target).replace("TARGET__", "")
-        fold_out = LASSO_CACHE / f"{fbase}_{tname}_folds.parquet"
+        fold_out = LGBM_CACHE / f"{fbase}_{tname}_folds.parquet"
 
         if fold_out.exists() and use_cached:
             fold_info = pd.read_parquet(fold_out)
@@ -199,15 +298,16 @@ def nested_lasso_feature_select(
                 desc = f"Outer fold {outer_fold + 1}"
                 inner_pbar.set_description(f"{desc:>{PBAR_PAD}}")
                 df_train, df_test = df_select.iloc[idx_train], df_select.iloc[idx_test]
-                results = lasso_select_target(
+                results = lgbm_select_target(
                     df_train=df_train,
                     df_test=df_test,
                     target=target,
                     regex=feature_regex,
+                    scoring=scoring,
+                    n_tune=n_tune,
                     bin_stratify=bin_stratify,
                 )
                 results.insert(2, "outer_fold", outer_fold + 1)
-                results.insert(3, "inner_fold", range(1, 6))
                 all_fold_results.append(results.copy())
                 inner_pbar.update()
             inner_pbar.reset()
@@ -230,11 +330,11 @@ if __name__ == "__main__":
     infos = []
     for regex in FeatureRegex:
         infos.append(
-            nested_lasso_feature_select(
+            nested_lgbm_feature_select(
                 dataset=data,
                 feature_regex=regex,
                 bin_stratify=True,
-                use_cached=True,
+                use_cached=False,
             )
         )
     info = pd.concat(infos, axis=0, ignore_index=True)
@@ -252,7 +352,7 @@ if __name__ == "__main__":
 
     print(bests.round(4).to_markdown(tablefmt="simple", index=False, floatfmt="0.4f"))
 
-    # see https://stats.stackexchange.com/questions/319861/how-to-interpret-lasso-shrinking-all-coefficients-to-0  # noqa
+    # see a lot of https://stats.stackexchange.com/questions/319861/how-to-interpret-lasso-shrinking-all-coefficients-to-0
     # basically, we don't have linear relationships
     dfs = []
     for _, df in info.groupby(["source", "target"]):
